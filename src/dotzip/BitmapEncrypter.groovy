830a9b8955b34ef7ee54fd958ad405c8851a87b9
dotzip <edmirzoev@yandex.ru>
2018-05-11 15:57:07 +0300

    + private func, setSize/getSize

--- src/dotzip/BitmapEncrypter.groovy
+++ src/dotzip/BitmapEncrypter.groovy
@@ -5,23 +5,14 @@ import java.nio.ByteBuffer
 
 class BitmapEncrypter {
     private BMP bmp
-    private char[] xMessageBinary
-    private char[] sizeBinaryMessage
+    private byte[] xMessageBytes
     private final byte ENCRYPTED_FILE_FLAG = "/".bytes[0] // {47}
 
-    BitmapEncrypter(String pathToFile, String xMessage){
-        this.bmp = new BMP()
-        this.xMessageBinary = bytesToBinary(xMessage.bytes)
-        this.sizeBinaryMessage = bytesToBinary(xMessageBinary.size().byteValue())
-        bmp.readBitmapPicture(pathToFile)
-    }
-
     BitmapEncrypter(String pathToFile){
-        this.bmp = new BMP()
-        bmp.readBitmapPicture(pathToFile)
+        this.bmp = new BMP() ; bmp.readBitmapPicture(pathToFile)
     }
 
-    def bytesToBinary = { byte[] inputByteArray ->
+    private def bytesToBinary = { byte[] inputByteArray ->
         char[] resArray = new char[inputByteArray.size() * 8], tempArray
         for (int i = 0; i < inputByteArray.size(); i++) {
             if (inputByteArray[i] < 0) {
@@ -38,7 +29,7 @@ class BitmapEncrypter {
         return resArray
     }
 
-    def binaryToBytes = { char[] bits ->
+    private def binaryToBytes = { char[] bits ->
         byte[] array = new byte[4]
         byte[] temp = new BigInteger(bits.toString(), 2).toByteArray()
         for (int i = 0; i < temp.size(); i++) {
@@ -50,9 +41,9 @@ class BitmapEncrypter {
         return array
     }
 
-    void setEncryptionFlag(){
+    private def setEncryptionFlag = {
         char[] encryptedFileFlagBinary = bytesToBinary(ENCRYPTED_FILE_FLAG), binaryColor
-        byte[] newRed = new byte[4], newGreen = new byte[4], newBlue = new byte[4]
+        byte[] newRed, newGreen, newBlue
         int[] colorArray = bmp.getPixelRGB(0, 0)
         // @red
         binaryColor = bytesToBinary(colorArray[0].byteValue())
@@ -79,9 +70,8 @@ class BitmapEncrypter {
     }
 
     boolean isEncrypted(){
+        char[] encryptedFileFlagBinary = new char[8], binaryColor
         int[] colorArray = bmp.getPixelRGB(0, 0)
-        char[] encryptedFileFlagBinary = new char[8]
-        char[] binaryColor = new char[8]
         // get needful bits from color pixel 0.0
         // @red
         binaryColor = bytesToBinary(colorArray[0].byteValue())
@@ -102,7 +92,70 @@ class BitmapEncrypter {
         return ByteBuffer.wrap(binaryToBytes(encryptedFileFlagBinary)).int == ENCRYPTED_FILE_FLAG
     }
 
-    def writeEncryptedBitmap(String path, String fileName){
+    private def setSizeOfXMessage = {
+        char[] sizeXMessage = bytesToBinary(xMessageBytes.size().byteValue()), binaryColor
+        byte[] newRed, newGreen, newBlue
+        int[] colorArray = bmp.getPixelRGB(0, 1)
+        // @red
+        binaryColor = bytesToBinary(colorArray[0].byteValue())
+        binaryColor[6] = sizeXMessage[0]
+        binaryColor[7] = sizeXMessage[1]
+        newRed = binaryToBytes(binaryColor)
+        // @green
+        binaryColor = bytesToBinary(colorArray[1].byteValue())
+        binaryColor[5] = sizeXMessage[2]
+        binaryColor[6] = sizeXMessage[3]
+        binaryColor[7] = sizeXMessage[4]
+        newGreen = binaryToBytes(binaryColor)
+        // @blue
+        binaryColor = bytesToBinary(colorArray[2].byteValue())
+        binaryColor[5] = sizeXMessage[5]
+        binaryColor[6] = sizeXMessage[6]
+        binaryColor[7] = sizeXMessage[7]
+        newBlue = binaryToBytes(binaryColor)
+
+        colorArray = [ByteBuffer.wrap(newRed).int,
+                      ByteBuffer.wrap(newGreen).int,
+                      ByteBuffer.wrap(newBlue).int]
+        bmp.setPixelRGB(0, 1, colorArray)
+    }
+
+    short getSizeOfXMessage(){
+        char[] sizeOfXMessage = new char[8], binaryColor
+        int[] colorArray = bmp.getPixelRGB(0, 1)
+        // get needful bits from color pixel 0.1
+        // @red
+        binaryColor = bytesToBinary(colorArray[0].byteValue())
+        sizeOfXMessage[0] = binaryColor[6]
+        sizeOfXMessage[1] = binaryColor[7]
+        // @green
+        binaryColor = bytesToBinary(colorArray[1].byteValue())
+        sizeOfXMessage[2] = binaryColor[5]
+        sizeOfXMessage[3] = binaryColor[6]
+        sizeOfXMessage[4] = binaryColor[7]
+        // @blue
+        binaryColor = bytesToBinary(colorArray[2].byteValue())
+        sizeOfXMessage[5] = binaryColor[5]
+        sizeOfXMessage[6] = binaryColor[6]
+        sizeOfXMessage[7] = binaryColor[7]
+
+        return ByteBuffer.wrap(binaryToBytes(sizeOfXMessage)).int
+    }
+
+    void encryptXMessage(String xMessage){
+        this.xMessageBytes = xMessage.bytes
+        char[] xMessageBinary = bytesToBinary(xMessageBytes)
+        if (this.encrypted) { println "Bitmap pic is already contained message!"; return }
+        else{ setEncryptionFlag() ; println "Successful setting flag!" }
+        setSizeOfXMessage()
+        /*for (int i = 0; i < bmp.bitmapWidth; i++) {
+            for (int j = 0; j < bmp.bitmapHeight; j++) {
+
+            }
+        }*/
+    }
+
+    void saveEncryptedBitmap(String path, String fileName) {
         ImageIO.write(bmp.bufferedImage, "BMP", new File("$path\\$fileName"))
     }
 }
